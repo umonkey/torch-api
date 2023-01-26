91f1fef8de07a50da769766cbc430acc8eeb3742
Justin Forest <hex@umonkey.net>
2023-01-26 16:54:17 +0400

    Add web bootstrap

--- config/routes.php
+++ config/routes.php
@@ -10,10 +10,13 @@ use App\Index\Actions\IndexAction;
 use App\OAuth\Actions\PasswordGrantAction;
 use App\Pages\Actions\GetPageAction;
 use App\Pages\Actions\PutPageAction;
+use Slim\Routing\RouteCollectorProxy;
 
-$app->get('/', IndexAction::class);
+$app->group('/api/v1', function (RouteCollectorProxy $group) {
+    $group->get('/', IndexAction::class);
 
-$app->post('/oauth/password', PasswordGrantAction::class);
+    $group->post('/oauth/password', PasswordGrantAction::class);
 
-$app->get('/pages/{id}', GetPageAction::class);
-$app->put('/pages/{id}', PutPageAction::class);
+    $group->get('/pages/{id}', GetPageAction::class);
+    $group->put('/pages/{id}', PutPageAction::class);
+});
--- src/bootstrap-web.php
+++ src/bootstrap-web.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-define('APP_ENV', getenv('APP_ENV'));
+define('APP_ENV', match (getenv('APP_ENV')) {
+    false => 'production',
+    default => getenv('APP_ENV'),
+});
 
 App\App::run();
